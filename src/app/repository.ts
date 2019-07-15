3f3259b832dcc52bf8f41e4d9f1e874934656082
Laura Macharia <lauramacharia117@gmail.com>
2019-07-15 09:35:43 +0300

    git-search

---
+++ src/app/repository.ts
@@ -0,0 +1,7 @@
+export class Repository {
+    constructor(
+        public name:string,
+        public http_url:string,
+        public description:string
+    ) {}
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:"https://api.github.com/users/",
+  token:"c5d699e6c6c8644ac0c40930a23aaf26681d7fad",
 };
